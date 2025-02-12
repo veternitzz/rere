8351c3521c500b02e384101e109d2ec1d2cd74a9
Ashton Scott Snapp <ashton@ashtonsnapp.com>
2025-02-12 10:53:26 -0600

    Started on runtime and made module for stdlib.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+//--> Imports & Modules <--
+
+mod libs;
+
+use std;
+
+use clap::{Command, Arg};
+
+use tokio;
+
+use mlua::{Lua, Compiler, StdLib, LuaOptions};
+
+//--> Type Aliases <--
+
+//--> Structs <--
+
+//--> Enums <--
+
+//--> Functions & Impls <--
+
 fn main() {
-    println!("Hello, world!");
+    // DO CLAP THINGS
+    let matches = Command::new("rere")
+        .get_matches();
+
+    // DO MLUA THINGS
+    let mut luau = Lua::new_with(StdLib::NONE, LuaOptions::default()).unwrap();
+    luau.set_compiler(Compiler::new()
+        .set_optimization_level(1)
+        .set_coverage_level(2));
+
+    // DO TOKIO THINGS
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {})
 }
